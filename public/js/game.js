3ebb5993ef78aaa284748fdaf93d4ffdc9cd87d4
Christian <carl_hassel@hotmail.com>
2021-06-21 13:18:30 +0200

    sample load testing

--- public/js/game.js
+++ public/js/game.js
@@ -213,6 +213,7 @@ const Game = (() => {
         });
         //Tone.Buffer.loaded().then(callback());
         instrument.toDestination();
+        callback();
     }
 
     function getPlayer () {
